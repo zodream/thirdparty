96904684d8313b59fa33d73a59324f1b3855f49a
ZoDream <648383079@qq.com>
2019-08-31 22:09:26 +0800

    修改

--- src/ThirdParty.php
+++ src/ThirdParty.php
@@ -10,10 +10,11 @@ namespace Zodream\ThirdParty;
 use Zodream\Helpers\Str;
 use Zodream\Http\Http;
 use Zodream\Http\Uri;
+use Zodream\ThirdParty\Traits\Attributes;
 
 abstract class ThirdParty {
 
-    protected $__attributes = [];
+    use Attributes;
 
     /**
      * KEY IN CONFIG
@@ -73,63 +74,7 @@ abstract class ThirdParty {
         return $http->text();
     }
 
-    /**
-     * 合并数组并返回新数组
-     * @param array $data
-     * @return array
-     */
-    public function merge(array $data) {
-        return array_merge($this->__attributes, $data);
-    }
 
-    public function get($key = null, $default = null) {
-        if (empty($key)) {
-            return $this->__attributes;
-        }
-        if (!is_array($this->__attributes)) {
-            $this->__attributes = (array)$this->__attributes;
-        }
-        if ($this->has($key)) {
-            return $this->__attributes[$key];
-        }
-        return $default;
-    }
-
-    /**
-     * 判断是否有
-     * @param string|null $key 如果为null 则判断是否有数据
-     * @return bool
-     */
-    public function has($key = null) {
-        if (is_null($key)) {
-            return !empty($this->__attributes);
-        }
-        if (empty($this->__attributes)) {
-            return false;
-        }
-        return isset($this->__attributes[$key]) || array_key_exists($key, $this->__attributes);
-    }
-
-    /**
-     * 设置值
-     * @param string|array $key
-     * @param string $value
-     * @return $this
-     */
-    public function set($key, $value = null) {
-        if (is_object($key)) {
-            $key = (array)$key;
-        }
-        if (is_array($key)) {
-            $this->__attributes = array_merge($this->__attributes, $key);
-            return $this;
-        }
-        if (empty($key)) {
-            return $this;
-        }
-        $this->__attributes[$key] = $value;
-        return $this;
-    }
 
     /**
      * _call
---
+++ src/Traits/Attributes.php
@@ -0,0 +1,96 @@
+<?php
+namespace Zodream\ThirdParty\Traits;
+
+
+trait Attributes {
+
+    protected $__attributes = [];
+
+    /**
+     * 合并数组并返回新数组
+     * @param array $data
+     * @return array
+     */
+    public function merge(array $data) {
+        return array_merge($this->__attributes, $data);
+    }
+
+    public function get($key = null, $default = null) {
+        if (empty($key)) {
+            return $this->__attributes;
+        }
+        if (!is_array($this->__attributes)) {
+            $this->__attributes = (array)$this->__attributes;
+        }
+        if (method_exists($this, 'preProcessKey')) {
+            $key = $this->preProcessKey($key);
+        }
+        if ($this->has($key)) {
+            return $this->__attributes[$key];
+        }
+        return $default;
+    }
+
+    /**
+     * 判断是否有
+     * @param string|null $key 如果为null 则判断是否有数据
+     * @return bool
+     */
+    public function has($key = null) {
+        if (is_null($key)) {
+            return !empty($this->__attributes);
+        }
+        if (empty($this->__attributes)) {
+            return false;
+        }
+        if (method_exists($this, 'preProcessKey')) {
+            $key = $this->preProcessKey($key);
+        }
+        return isset($this->__attributes[$key]) || array_key_exists($key, $this->__attributes);
+    }
+
+    /**
+     * 设置值
+     * @param string|array $key
+     * @param string $value
+     * @return $this
+     */
+    public function set($key, $value = null) {
+        if (is_object($key)) {
+            $key = (array)$key;
+        }
+        if (is_array($key)) {
+            foreach ($key as $k => $val) {
+                if (method_exists($this, 'preProcessKey')) {
+                    $k = $this->preProcessKey($k);
+                }
+                $this->__attributes[$k] = $value;
+            }
+            return $this;
+        }
+        if (empty($key)) {
+            return $this;
+        }
+        if (method_exists($this, 'preProcessKey')) {
+            $key = $this->preProcessKey($key);
+        }
+        $this->__attributes[$key] = $value;
+        return $this;
+    }
+
+    public function offsetGet($offset) {
+        return $this->get($offset);
+    }
+
+    public function __get($attribute) {
+        return $this->get($attribute);
+    }
+
+    public function offsetSet($offset, $value) {
+        $this->set($offset, $value);
+    }
+
+    public function __set($attribute, $value) {
+        $this->set($attribute, $value);
+    }
+}
