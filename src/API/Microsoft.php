48bdab05530c36f57256790c0c555a995ed20c6d
ZoDream <648383079@qq.com>
2023-03-17 14:13:50 +0800

    增加必应壁纸API

--- src/API/Microsoft.php
+++ src/API/Microsoft.php
@@ -24,6 +24,15 @@ class Microsoft extends ThirdParty  {
         return $this->getHttp('http://kan.msxiaobing.com/Api/Image/UploadBase64');
     }
 
+    public function getWallpager() {
+        return $this->getHttp()
+            ->url('https://cn.bing.com/HPImageArchive.aspx?format=js', [
+                'idx' => 0,
+                'n' => 1,
+                'mkt' =>'zh-CN'
+            ]);
+    }
+
     /**
      * 颜值测试
      * @param string $img base64_encode
@@ -42,5 +51,45 @@ class Microsoft extends ThirdParty  {
         ));
         return $this->getFaceScore()
             ->parameters($this->get())->json();
+    }
+
+    /**
+     * 获取壁纸
+     * @param int $amount
+     * @param int $dayAgo -1 表示明天，> 1 表示昨天以前
+     * @return array{url: string,m_url: string,title: string}[]
+     * @throws \Exception
+     */
+    public function wallpager(int $amount = 1, int $dayAgo = 0) {
+        if ($amount > 8) {
+            $amount = 8;
+        } elseif ($amount < 1) {
+            $amount = 1;
+        }
+        if ($dayAgo > 16) {
+            $dayAgo = 16;
+        } elseif ($dayAgo < -1) {
+            $dayAgo = -1;
+        }
+        $data = $this->getWallpager()
+            ->parameters([
+                'n' => $amount,
+                'idx' => $dayAgo
+            ])->json();
+        if (empty($data) || empty($data['images'])) {
+            return [];
+        }
+        $baseHost = 'https://cn.bing.com';
+        return array_map(function ($item) use ($baseHost) {
+            return [
+                'url' => $baseHost. $item['url'],
+                'm_url' => $this->wallpagerUrl($baseHost. $item['urlbase'], 1080, 1920),
+                'title' => $item['title'],
+            ];
+        }, $data['images']);
+    }
+
+    private function wallpagerUrl(string $base, int $width, int $height): string {
+        return  sprintf('%s_%dx%d.jpg&rf=LaDigue_%dx%d.jpg&pid=hp', $base, $width, $height, $width, $height);
     }
 }
