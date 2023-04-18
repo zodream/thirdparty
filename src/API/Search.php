3a8d57d36b49738fe383846831b80e6010f4befe
ZoDream <648383079@qq.com>
2023-04-18 17:51:25 +0800

    add indexnow

--- src/API/Search.php
+++ src/API/Search.php
@@ -10,6 +10,8 @@ use Zodream\ThirdParty\ThirdParty;
 
 class Search extends ThirdParty {
 
+    protected string $configKey = 'search';
+
     public function getBaiDu() {
         return $this->getHttp()
             ->url('http://data.zz.baidu.com/urls', [
@@ -36,5 +38,33 @@ class Search extends ThirdParty {
         })->json();
     }
 
-
+    /**
+     * 使用indexnow提交网址
+     * @param array|string $urls
+     * @param string $searchEngine
+     * @return bool
+     * @throws \Exception
+     */
+    public function indexNow(array|string $urls, string $searchEngine = 'www.bing.com'): bool {
+        if (empty($searchEngine) || empty($urls)) {
+            return false;
+        }
+        $postUri = sprintf('https://%s/indexnow', $searchEngine);
+        $http = $this->getHttp();
+        if (!is_array($urls)) {
+            $http->url($postUri, [
+                    'url' => $urls,
+                    'key' => $this->get('bing_indexnow'),
+                    'keyLocation'
+                ])->get();
+        } else {
+            $http->url($postUri)->maps([
+                    'host' => parse_url($urls[0], PHP_URL_HOST),
+                    'key' => $this->get('bing_indexnow'),
+                    'urlList' => $urls,
+                    'keyLocation'
+                ])->encode()->post();
+        }
+        return $http->getStatusCode() === 200;
+    }
 }
